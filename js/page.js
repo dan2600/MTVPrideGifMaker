28894af78ca061a07e4c5081f6b92f18eb405c22
Dan Spagnoli <Dan.Spagnoli@vh1.com>
2017-05-24 18:05:07 -0400

    updated text and added some features

--- js/page.js
+++ js/page.js
@@ -1,5 +1,10 @@
 //Main Card Maker Function
 function makeCard() {
+    document.getElementsByClassName("result")[0].innerHTML = "";
+    loader = document.createElement('img');
+    loader.src = 'images/ring.svg';
+    document.getElementsByClassName("result")[0].appendChild(loader);
+
     var images = [];
     var main = document.createElement('canvas');
     main.width = 470;
@@ -16,29 +21,67 @@ function makeCard() {
     }, function(obj) {
         if (!obj.error) {
             var image = obj.image,
-                animatedImage = document.createElement('img');
+            animatedImage = document.createElement('img');
             animatedImage.src = image;
-            document.getElementsByClassName("content")[0].appendChild(animatedImage);
+            animatedImage.className += "resultgif";
+            document.getElementsByClassName("result")[0].innerHTML = "";
+            document.getElementsByClassName("result")[0].appendChild(animatedImage);
+                               document.getElementsByClassName("result")[0].appendChild(document.createElement("br"))
+            var node = document.createElement("button");                 // Create a <li> node
+            var textnode = document.createTextNode("Download Image");
+            node.appendChild(textnode);
+            node.onclick = function() {downloadURI(image, "pride.gif")};  
+            document.getElementsByClassName("result")[0].appendChild(node);
+
+            
         }
     });
-    300
 
 }
 
+function downloadURI(uri, name) {
+  var link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
+}
+
+
 //Makes the Rainbow Effect and Applies Text
 function rainbowmaker(context, shift) {
-    words = ["Proud to Be", document.getElementById('p0').value + "&", document.getElementById('p1').value + "&", document.getElementById('p2').value + "&", document.getElementById('p3').value + "&", "ME"];
+    function ander(s)
+    {
+        if(s !== "")
+        {
+            return "&";
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    words = ["Proud to Be", document.getElementById('p0').value + ander(document.getElementById('p0').value), document.getElementById('p1').value + ander(document.getElementById('p1').value), document.getElementById('p2').value + ander(document.getElementById('p2').value), document.getElementById('p3').value + ander(document.getElementById('p3').value), "standing with the LGBTQ community."];
     colors = ["#f40000", "#ff8601", "#faf601", "#01881f", "#4600ff", "#830189"];
     n = colors.length;
     for (z = 0, j = 0 + shift; z < n; z++, j++) {
         if (j >= n) {
             j = 0;
         }
-        res = (colors[j]);
-        context.fillStyle = res;
+        blockColor = (colors[j]);
+        context.fillStyle = blockColor;
         context.fillRect(0, 78.33 * z, 470, 78.33);
         context.fillStyle = "#000";
+        if(z === 5)
+        {
+         context.font = "26.5px Arial";   
+        }
+        else{
         context.font = "50px Arial";
+        }
         context.fillText(words[z], 25, (78.33 * (z + 1)) - 25);
     }
 }
